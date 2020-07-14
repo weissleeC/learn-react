baffa98266791cc2a8889fff3cc9efe1086509f1
[v_shilili] <v_shilili@tencent.com>
2020-07-14 18:50:13 +0800

    feat:router

--- src/RouterDom.jsx
+++ src/RouterDom.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 /**
  * 在但页面应用中使用 Router 只是在 body 下渲染组件，并不是重新跳转页面渲染
+ * 
+ * exact: 展示对应的组件，严格匹配。
+ * 
+ * strict: 更精准的匹配路由。例如：http://localhost:3000/about [能访问]。http://localhost:3000/about/ [无法访问]
  */
 
 export default class RouterDom extends React.Component {
@@ -18,12 +23,14 @@ export default class RouterDom extends React.Component {
       <Router>
         <nav>
           <ul>
-            <li><Link to="/home">home</Link></li>
+            <li><Link to="/">home</Link></li>
             <li><Link to="/about">about</Link></li>
+            <li><Link to="/about/contact">contact</Link></li>
           </ul>
         </nav>
-        <Route path="/home" component={ Home }></Route>
-        <Route path="/about" component={ About }></Route>
+        <Route exact path="/" component={ Home }></Route>
+        <Route exact strict path="/about" component={ About }></Route>
+        <Route path="/about/contact" component={ Contact }></Route>
       </Router>
     )
   }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div>hi, contact</div>
+    )
+}
+
+export default Contact;
