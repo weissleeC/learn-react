43d6a235b7353db297986936855fc0ed14fc4016
[v_shilili] <v_shilili@tencent.com>
2020-07-31 18:23:07 +0800

    fix:css

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import React,{ Fragment } from 'react';
 // import RefsDOM from './RefsDOM';
 // import AntdButon from './AntdButton'
 // import FetchDOM from './FetchDOM';
+import Update from './Update';
 // import RouterDom from './RouterDom';
-import ReduxDOM from './ReduxDOM';
+// import ReduxDOM from './ReduxDOM';
 
 export default class App extends React.Component{
 
   render() {
     return(
       <Fragment>
-        <ReduxDOM />
+        <Update />
       </Fragment>
     )
   }
---
+++ src/Update.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function Update() {
+  return(
+		<div>上传功能</div>
+	)
+}
