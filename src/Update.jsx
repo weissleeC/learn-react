6a5c368882267fc8f81a3aaffc0a070d94e68bbd
[v_shilili] <v_shilili@tencent.com>
2020-08-03 19:26:14 +0800

    fix:css

--- src/Update.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-export default function Update() {
-  return(
-		<div>上传功能</div>
-	)
-}
---
+++ src/UploadMap.jsx
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+
+import { Upload, message, Button } from 'antd';
+
+
+export default function UploadMap() {
+
+  const props = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  return(
+    <Fragment>
+      <Upload {...props}>
+        <Button>点击上传</Button>
+      </Upload>
+    </Fragment>
+	)
+}
